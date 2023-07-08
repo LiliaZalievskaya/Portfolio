a8ef5fb590c9639ced1aa8797c6e900392147df8
LiliaZalievskaya <138985936+LiliaZalievskaya@users.noreply.github.com>
2023-07-08 22:39:16 +0300

    Add files via upload

--- script/app.js
+++ script/app.js
@@ -8,3 +8,22 @@ VANTA.RINGS({
   scale: 1.00,
   scaleMobile: 1.00
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+  var header = document.querySelector(".header");
+  setTimeout(function() {
+    header.classList.add("show-header");
+  });
+});
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+function scrollToTop() {
+  if ('scrollBehavior' in document.documentElement.style) {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  } else {
+    window.scroll(0, 0);
+  }
+}
